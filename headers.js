80a3e3677cda5c0dbb92462848c90587a1df68d8
Ben Edridge <BenEdridge@users.noreply.github.com>
2019-04-06 00:50:29 +1100

    Added skeleton repo

---
+++ headers.js
@@ -0,0 +1,7 @@
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+};
+
+module.exports = {
+    defaultHeaders
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const headers = require('./headers');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, headers.defaultHeaders);
+
+    if(req.url === '/'){
+        res.write(JSON.stringify({ response: "Welcome" }));
+    } else {
+        res.write(JSON.stringify({ response : "Consult /" }));
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server listening");
+});
